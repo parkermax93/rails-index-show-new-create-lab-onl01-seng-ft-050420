1b1b26785e1f20346487edd2df2ff57dd74242fa
Parker Williamson <parker.williamson@gmail.com>
2020-07-14 17:32:35 +0000

    Done.

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,21 @@
+class CouponsController < ApplicationController
+    def index
+        @coupons = Coupon.all
+    end
+
+    def show
+        set_coupon
+    end
+
+    def create
+        @coupon = Coupon.create(coupon_code: params[:coupon][:coupon_code], store: params[:coupon][:store])
+        redirect_to coupon_path(@coupon)
+    end
+
+    private
+
+        def set_coupon
+            @coupon = Coupon.find_by_id(params[:id])
+        end
+
+end
